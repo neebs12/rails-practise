340b272c2a3edcbe7197b8d870a7841187928921
Jason Aricheta <jason@hnry.io>
2023-02-28 10:13:58 +1300

    added more routes and erb manipulation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  # identical name of template is expected at app/views/articles/another_index.html.erb ...
+  def show
+    @article = Article.find(params[:id])
   end
 end
---
+++ app/models/article.rb
@@ -0,0 +1,3 @@
+# this is auto-loaded in to other locations in the repo!
+class Article < ApplicationRecord
+end
