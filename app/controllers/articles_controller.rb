d935238d435c4d7e32be80c6809a1dce4544f320
Jason Aricheta <jason@hnry.io>
2023-03-01 08:46:06 +1300

    completed article create and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,47 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-
+  
   # identical name of template is expected at app/views/articles/another_index.html.erb ...
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def new 
+    # this `@article` is used in rendering for `new.html.erb`! This is then inputted in to form_with method of the overarching context. `form_with model: @article`.. then this is ""taken"" apart by do |form|, where form had the link to the `@article`. Then it knows to point to the model and the correct column in the model via the `form.label :title` - this is because the symbol name is the same to that of the migration's
+    @article = Article.new
+  end
+
+  # but for some methods which are not mapped to a GET. These actions are not mapped to a view!
+  def create 
+    @article = Article.new(article_params)
+
+    if @article.save # sees if successful!
+      redirect_to @article
+    else # if the new article is unsuccessful!
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    # this updates the 
+    if @article.update(article_params)
+      # this pulls up the model as it exists in the database
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    # appears to be mapping to `form.text_area :symbol`
+    params.require(:article).permit(:title, :body)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 # this is auto-loaded in to other locations in the repo!
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
